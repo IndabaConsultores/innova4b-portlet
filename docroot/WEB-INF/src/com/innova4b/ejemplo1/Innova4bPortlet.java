dc6be96fa9bd36a7aca4d959f1f32259f15abb2a
aritzg <galdos.aritz@gmail.com>
2015-04-17 09:59:02 +0200

    Understanding portlet phases

--- docroot/WEB-INF/src/com/innova4b/ejemplo1/Innova4bPortlet.java
+++ docroot/WEB-INF/src/com/innova4b/ejemplo1/Innova4bPortlet.java
@@ -2,9 +2,13 @@ package com.innova4b.ejemplo1;
 
 import java.io.IOException;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -18,6 +22,21 @@ public class Innova4bPortlet extends MVCPortlet {
 			RenderResponse renderResponse) throws IOException, PortletException {
 		_log.debug("Estamos en doView");
 		super.doView(renderRequest, renderResponse);
+	}
+	
+	public void accion1(ActionRequest actionRequest, ActionResponse actionResponse){
+		String valor1 = ParamUtil.getString(actionRequest, "valor1","");
+		if(!StringUtils.isEmpty(valor1)){
+			_log.debug("Accion1: Guardamos en BDD el valor " +  valor1);
+		}
+		
+	}
+	
+	public void accion2(ActionRequest actionRequest, ActionResponse actionResponse){
+		String valor2 = ParamUtil.getString(actionRequest, "valor2","");
+		if(!StringUtils.isEmpty(valor2)){
+			_log.debug("Accion2: Guardamos en BDD el valor " +  valor2);
+		}
 	}
 	
 	Log _log = LogFactoryUtil.getLog(this.getClass());
--- docroot/html/ejemplo1/view.jsp
+++ docroot/html/ejemplo1/view.jsp
@@ -18,3 +18,31 @@ String saludo = portletPreferences.getValue("saludo", "Hola ");
 <%if(!StringUtils.isEmpty(nombre)){ %>
 <%=saludo %> <%=nombre%>!
 <%} %>
+
+
+<hr/>
+
+<liferay-portlet:actionURL  var="accion1URL" name="accion1"/>
+<aui:form action="<%= accion1URL %>" method="post" name="fm1">
+
+    <aui:input name="valor1" type="text"/>
+
+    <aui:button-row>
+       <aui:button type="submit" />
+    </aui:button-row>
+</aui:form>
+
+<hr/>
+
+<liferay-portlet:actionURL  var="accion2URL" name="accion2"/>
+<aui:form action="<%= accion2URL %>" method="post" name="fm2">
+
+    <aui:input name="valor2" type="text"/>
+
+    <aui:button-row>
+       <aui:button type="submit" />
+    </aui:button-row>
+</aui:form>
+
+
+
